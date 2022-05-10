f8c20df64c45036156895ea8c8cffa7bcb300504
EthanSystem <ElementsPhoenix@outlook.com>
2022-05-10 20:06:46 +0800

    reinit 2022-05-10

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ElementalCognitionSystem
+//
+//  Created by Ethan on 2022/04/19.
+//
+
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
